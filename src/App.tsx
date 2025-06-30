fb1a193866b5dc600c0a6db395b1c191bdc3f326
Danil <danil.piat@mail.ru>
2025-06-30 23:33:08 +0300

    Фикс чтобы фон не скакал

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,16 @@ import CheckoutPage from './pages/CheckoutPage/CheckoutPage';
 function App() {
   return (
   <CartProvider>
+    <div className="background-image"></div>
     <div className="app">
-      <Header />
+      <Header/>
       <Routes>
-        <Route path="/" element={<Navigate to="/catalog" replace />} />
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/catalog/categories" element={<CatalogCategoriesPage />} />
-        <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="/cart" element={<CartPage />} /> {/* Обновленный роут */}
-        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/" element={<Navigate to="/catalog" replace/>}/>
+        <Route path="/catalog" element={<CatalogPage/>}/>
+        <Route path="/catalog/categories" element={<CatalogCategoriesPage/>}/>
+        <Route path="/product/:id" element={<ProductPage/>}/>
+        <Route path="/cart" element={<CartPage/>}/> {/* Обновленный роут */}
+        <Route path="/checkout" element={<CheckoutPage/>}/>
       </Routes>
     </div>
   </CartProvider>
